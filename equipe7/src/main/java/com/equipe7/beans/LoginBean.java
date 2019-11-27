9570627671f1c0fb528595cc36dd93aece3f0164
LuisMarcelo <mar0580@gmail.com>
2019-11-27 12:31:52 -0300

    Primeira Versão
    Setup necessário:  Tomcat 9, openjdk 1.8.0_232

---
+++ equipe7/src/main/java/com/equipe7/beans/LoginBean.java
@@ -0,0 +1,68 @@
+package com.equipe7.beans;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import com.equipe7.dao.LoginDAO;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 1094801825228386363L;
+
+	private String password;
+	private String message;
+	private String user;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	// validate login
+	public String validateUsernamePassword() {
+		boolean valid = LoginDAO.validate(user, password);
+		if (valid) {
+			HttpSession session = SessionUtils.getSession();
+			session.setAttribute("username", user);
+			return "admin";
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN,
+					"Incorrect Username and Passowrd", "Please enter correct username and Password"));
+			return "login";
+		}
+	}
+
+	// logout event, invalidate session
+	public String logout() {
+		HttpSession session = SessionUtils.getSession();
+		session.invalidate();
+		return "login";
+	}
+
+}
